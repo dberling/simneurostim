Fix unit of impedance to GOhm in beta calculation

base-neurostim/neurostim/chrimson_conductance_model.py
@@ -32,6 +32,7 @@ def ChRsystem_step(state: np.ndarray, I: float, sampling_period: float) -> np.nd
     #print(type(_O1))
 
     return np.array([_O1,_O2,_C1,_C2,_S]) * sampling_period
+
 def ChR_integration(y0, intensities, sampling_period):
     """
     Intensities contains intensity sampled at sampling_period.
@@ -124,7 +125,7 @@ def calc_rescaled_comp_conductances_nS(
     stimulator_config: list of dict
         MultiStimulator configuration
     comp_data: list
-        data per compartment: secname, sec_x, transfer_resistance_unit?, x, y, z, area_um2, channel_density_PERcm2
+        data per compartment: secname, sec_x, transfer_resistance_MOhm, x, y, z, area_um2, channel_density_PERcm2
     temp_protocol: dict
         duration_ms: int, delay_ms: int, total_rec_time_ms: int
     reject_if_sampling_smaller: float
@@ -149,10 +150,10 @@ def calc_rescaled_comp_conductances_nS(
     photon_flux_source_PER_fs = photon_flux_source_PER_s * 1e-15
     
     # load neuron comp data
-    secname, sec_x, transfer_resistance_unittobedef, x, y, z, area_um2, channel_density_PERcm2 = comp_data.T
-    #transfer_resistance_GOhm = transfer_resistance_MOhm * 1e-3
-    #soma_input_resistance_GOhm = transfer_resistance_GOhm[(secname==1) & (sec_x==0.5)].item()
-    soma_input_resistance = transfer_resistance_unittobedef[(secname==1) & (sec_x==0.5)].item()
+    secname, sec_x, transfer_resistance_MOhm, x, y, z, area_um2, channel_density_PERcm2 = comp_data.T
+    # since conductance is given in nS, convert resistances to GOhm
+    transfer_resistance_GOhm = transfer_resistance_MOhm * 1e-3
+    soma_input_resistance = transfer_resistance_GOhm[(secname==1) & (sec_x==0.5)].item()
     N_channel = area_um2 * 1e-8 * channel_density_PERcm2
     
     # load stimulator model
@@ -185,30 +186,6 @@ def calc_rescaled_comp_conductances_nS(
         sampling_period=interpol_dt_ms
     )
     comp_conductance_nS = channel_conductance_nS * np.array(N_channel).reshape((1,len(secname)))
-    rescaled_cond_nS = comp_conductance_nS / (1 + np.abs(np.array(transfer_resistance_unittobedef).reshape((1,len(secname))) - soma_input_resistance) * comp_conductance_nS)
-    rescaled_cond_nS_scaled = comp_conductance_nS / (1 + np.abs(np.array(transfer_resistance_unittobedef).reshape((1,len(secname))) * scale_imp - soma_input_resistance) * comp_conductance_nS)
+    rescaled_cond_nS = comp_conductance_nS / (1 + np.abs(np.array(transfer_resistance_GOhm).reshape((1,len(secname))) - soma_input_resistance) * comp_conductance_nS)
+    rescaled_cond_nS_scaled = comp_conductance_nS / (1 + np.abs(np.array(transfer_resistance_GOhm).reshape((1,len(secname))) * scale_imp - soma_input_resistance) * comp_conductance_nS)
     return rescaled_cond_nS, rescaled_cond_nS_scaled, interpol_dt_ms, True
-
-def calc_rescaled_comp_conductances_nS_with_GOhm_impedances(
-    norm_power_mW_of_MultiStimulator, 
-    stimulator_config,
-    comp_data, 
-    temp_protocol,
-    scale_imp,
-    reject_if_sampling_smaller=0.001
-):
-
-    secname, sec_x, transfer_resistance_unittobedef, x, y, z, area_um2, channel_density_PERcm2 = comp_data.T
-    comp_data_GOhm_impedance = np.array(
-            [secname, sec_x, transfer_resistance_unittobedef*1e-3, 
-             x, y, z, area_um2, channel_density_PERcm2]
-            ).T
-    return calc_rescaled_comp_conductances_nS(
-        norm_power_mW_of_MultiStimulator, 
-        stimulator_config,
-        comp_data_GOhm_impedance, 
-        temp_protocol,
-        scale_imp,
-        reject_if_sampling_smaller=0.001
-    )
-